c5980db6e9f90bb2d5a2ccb49135061f39ccf178
Roger Li <rel7817@nyu.edu>
2023-03-28 19:10:54 -0400

    gliding + fast fall abilities

---
+++ Assets/Scripts/abilities.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abilities : MonoBehaviour
+{
+    public player player;
+
+    public bool glidingEnabled;
+    public bool fastFallEnabled;
+
+    public float glidingAirSpeed = 8f;
+    public float glidingGravityScale = 0.15f;
+    public float fastFallGravityScale = 10f;
+
+    // called after a KeyDown event for moving left/right while in air.
+    public void handleGlideStart() 
+    {
+        // check if player has gliding ability.
+        if (!glidingEnabled) {
+            Debug.Log("You cannot glide yet...");
+            return;
+        }
+
+        // start airgliding.
+        //      1. change airspeed
+        //      2. and gravity scale.
+        player.setAirSpeed(glidingAirSpeed);
+        player.setGravityScale(glidingGravityScale);
+    }
+
+    // called after a KeyUp event for left/right movement and in air or when player is grounded.
+    public void handleGlideEnd() 
+    {
+        player.resetAirSpeed();
+        player.resetGravityScale();
+    }
+
+    public void handleFastFallStart()
+    {
+        if (!fastFallEnabled) {
+            Debug.Log("You cannot fast fall yet...");
+            return;
+        }
+
+        player.setGravityScale(fastFallGravityScale);
+    }
+
+    public void handleFastFallEnd()
+    {
+        player.resetGravityScale();
+    }
+}
--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -7,7 +7,8 @@ public class player : MonoBehaviour
 
     public float groundspeed = 10;
 
-    public float airspeed = 5;
+    public const float defaultAirSpeed = 5;
+    public float defaultGravityScale = 1;
     public float jumpForce = 350;
 
     public LayerMask theGround;
@@ -15,7 +16,11 @@ public class player : MonoBehaviour
     bool grounded = false;
     private float xSpeed;
 
+
+    private float airspeed = 5;
+
     Rigidbody2D _rigidbody;
+    abilities _abilities;
 
     Animator _animator;
 
@@ -25,26 +30,64 @@ public class player : MonoBehaviour
     { 
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        _abilities = GetComponent<abilities>();
+
+        airspeed = defaultAirSpeed;
     }
 
-
+    private bool isGliding = false;
+    private bool isFastFalling = false;
     void FixedUpdate() {
+
         if(grounded && state == 0)
         {
             xSpeed = Input.GetAxis("Horizontal") * groundspeed;
             _rigidbody.velocity = new Vector2(xSpeed, _rigidbody.velocity.y);
         }
-        else if(!grounded && state == 1)
+        else if (!grounded && state == 1)
         {
+            // if falling, handle possible gliding...
+            if (_rigidbody.velocity.y < 0f) {
+                if (Input.GetAxis("Horizontal") != 0f && !isGliding) {
+                    _abilities.handleGlideStart();
+                    isGliding = true;
+                } else if (Input.GetAxis("Horizontal") == 0f && isGliding) {
+                    _abilities.handleGlideEnd();
+                    isGliding = false;
+                } 
+            }
+
             xSpeed = Input.GetAxis("Horizontal") * airspeed;
             _rigidbody.velocity = new Vector2(xSpeed, _rigidbody.velocity.y);
         }
-        
+        // end gliding when grounded (reset airspeed and gravity)
+        else if (grounded && isGliding) 
+        {
+            if (state == 1) { // if square, stop all x movement.
+                _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
+            }
+            _abilities.handleGlideEnd();
+            isGliding = false;
+        }
+
+
+        // start fast fall when in air and pressing down key 
+        if (!grounded && Input.GetAxis("Vertical") < 0f) {
+            _abilities.handleFastFallStart();
+            isFastFalling = true;
+        }
+        // end fast fall when grounded.
+        if (isFastFalling && grounded) {
+            _abilities.handleFastFallEnd();
+            isFastFalling = false;
+        }
     }
-    // Update is called once per frame
+
     void Update()
     {
+
         grounded = Physics2D.OverlapCircle(bottom.position, 0.1f, theGround);
+
         if(Input.GetKeyDown(KeyCode.Space) && grounded && state == 1)
         {
             _rigidbody.AddForce(new Vector2(0, jumpForce));
@@ -62,5 +105,23 @@ public class player : MonoBehaviour
                 state = 0;
             }
         }
+    }
+
+
+
+    public void setAirSpeed(float newAirSpeed) {
+        airspeed = newAirSpeed;
+    }
+    public void resetAirSpeed() {
+        airspeed = defaultAirSpeed;
+    }
+    public void setGravityScale(float newGravityScale) {
+        _rigidbody.gravityScale = newGravityScale;
+    }
+    public void resetGravityScale() {
+        _rigidbody.gravityScale = defaultGravityScale;
+    }
+    public bool getGroundedState() {
+        return grounded;
     }
 }
