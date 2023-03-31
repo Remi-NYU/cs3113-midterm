5542db8efeb5d2216aaca00ab0ce5f28904acfdf
Mewtwoshaurd <mewtwoshard@gmail.com>
2023-03-31 13:49:54 -0400

    building stuff
    building stuff, minor edits, etc

--- Assets/Scripts/EndGoal.cs
+++ Assets/Scripts/EndGoal.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class EndGoal : MonoBehaviour
 {
+    public string nextLevel;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
             GameObject.FindGameObjectWithTag("StateManager").GetComponent<StateManager>().SwitchState(GameState.Victory);
+            
         }
     }
 }
