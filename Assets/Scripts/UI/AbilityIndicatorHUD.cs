228b46a650cc4bb68e0f10489ec32787af970bab
Remi <70982674+Remi-NYU@users.noreply.github.com>
2023-04-05 15:04:25 -0400

    Added ability HUD

---
+++ Assets/Scripts/UI/AbilityIndicatorHUD.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AbilityIndicatorHUD : MonoBehaviour
+{
+    Image image;
+    TMP_Text text;
+    [SerializeField] Sprite rollSprite;
+    [SerializeField] Sprite jumpSprite;
+    [SerializeField] Sprite fallSprite;
+    [SerializeField] Sprite glideSprite;
+
+    void Start()
+    {
+        image = transform.GetChild(0).GetComponent<Image>();
+        text = transform.GetChild(1).GetComponent<TMP_Text>();
+        SetRoll();
+    }
+
+    public void SetRoll() {
+        image.sprite = rollSprite;
+        text.text = "Roll";
+    }
+
+    public void SetJump() {
+        image.sprite = jumpSprite;
+        text.text = "Jump";
+    }
+
+    public void SetFall() {
+        image.sprite = fallSprite;
+        text.text = "Fall";
+    }
+
+    public void SetGlide() {
+        image.sprite = glideSprite;
+        text.text = "Glide";
+    }
+}
