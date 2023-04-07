1d192a48fb50c2dfc455b5bf921ea21a7af2860c
Mewtwoshaurd <mewtwoshard@gmail.com>
2023-04-07 14:34:33 -0400

    Sounds, Unifying Styles, Etc
    Build Ready

--- Assets/Scripts/EndGoal.cs
+++ Assets/Scripts/EndGoal.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class EndGoal : MonoBehaviour
 {
     public string nextLevel;
+    
+    public GameObject cameraAudio;
+    AudioSource audioSource;
+    
+    void Start() {
+       audioSource = GetComponent<AudioSource>();
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
+            cameraAudio.GetComponent<AudioSource>().Stop();
+            audioSource.Play();
             GameObject.FindGameObjectWithTag("StateManager").GetComponent<StateManager>().SwitchState(GameState.Victory);
-            
         }
     }
 }
