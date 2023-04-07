a18956e185c6e53cefc5ab5d17e7637cf56b2d5b
Remi <70982674+Remi-NYU@users.noreply.github.com>
2023-04-07 10:43:14 -0400

    Eye tracking for controller

--- Assets/Scripts/Eyes.cs
+++ Assets/Scripts/Eyes.cs
@@ -16,12 +16,19 @@ public class Eyes : MonoBehaviour
 
     void Update()
     {
-        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 camPos = cam.transform.position;
-        Vector3 pos = (mousePos - camPos);
-        pos.x = 2 * (1 / (1 + Mathf.Exp(-pos.x))) - 1;
-        pos.y = 2 * (1 / (1 + Mathf.Exp(-pos.y))) - 1;
-        pos.z = 0;
-        eyes.localPosition = pos * maxDistance;
+        if (Input.GetJoystickNames().Length == 0)
+        {
+            Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 camPos = cam.transform.position;
+            Vector3 pos = (mousePos - camPos);
+            pos.x = 2 * (1 / (1 + Mathf.Exp(-pos.x))) - 1;
+            pos.y = 2 * (1 / (1 + Mathf.Exp(-pos.y))) - 1;
+            pos.z = 0;
+            eyes.localPosition = pos * maxDistance;
+        }
+        else {
+            Vector3 pos = new Vector3(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"), 0);
+            eyes.localPosition = pos * maxDistance;
+        }
     }
 }
